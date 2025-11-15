13faa49ce83b353ca4abf417899760e460a600a3
Aleks-Ulya <alekseeva4547@gmail.com>
2025-11-15 13:11:36 +0300

    Update код.cpp

--- лр2/код.cpp
+++ лр2/код.cpp
@@ -7,18 +7,18 @@ struct Node {
 
 // Создание очереди:
 std::queue<std::string> queue;
-queue.push("Tom"); 
-queue.push("Bob"); 
-queue.push("Sam");
+queue.push("Ulya"); 
+queue.push("Alya"); 
+queue.push("Sonya");
 
 //Создание дека:
 std::deque<int> d = {1, 2, 3};
 
 //Создание приоритетной очереди с примитивным типом:
 std::priority_queue<int> pq;
-pq.push(10);     
-pq.push(20);    
-pq.push(15); 
+pq.push(20);     
+pq.push(40);    
+pq.push(5); 
 
 //Создание приоритетной очереди с пользовательскими структурами:
 std::priority_queue<Task> taskQueue;
